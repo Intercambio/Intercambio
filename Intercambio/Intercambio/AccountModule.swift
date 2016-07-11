3f1d65efadfe5e20631fa4a0a7a6d98fc370128a
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-07-11 22:49:10 +0200

    Implement constructor for the account module

--- Intercambio/Intercambio/AccountModule.swift
+++ Intercambio/Intercambio/AccountModule.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2016 Tobias Kräntzer. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import IntercambioCore
+
+public class AccountModule : NSObject {
+    
+    private let service: CommunicationService
+    private let router: AccountModuleRouter
+    
+    init(service: CommunicationService, router: AccountModuleRouter) {
+        self.service = service
+        self.router = router
+    }
+    
+    public func viewController(uri: URL) -> (UIViewController?) {
+        
+        if let host = uri.host,
+            let jid = JID(user: uri.user, host: host, resource: nil) {
+            
+            let interactor = AccountModuleInteractorImpl(accountJID: jid,
+                                                         keyChain: service.keyChain,
+                                                         accountManager: service.accountManager)
+            
+            let presenter = AccountModulePresenterImpl()
+            let viewControler = AccountModuleViewController()
+            
+            // strong references (view controller -> presenter -> interactor)
+            viewControler.eventHandler = presenter
+            presenter.interactor = interactor
+            
+            // weak references
+            interactor.presenter = presenter
+            presenter.userInterface = viewControler
+            presenter.router = router
+            
+            return viewControler
+        } else {
+            return nil
+        }
+    }
+}
