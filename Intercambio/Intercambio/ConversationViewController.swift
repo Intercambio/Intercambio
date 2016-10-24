3df83c96841201dcccd987df606e8e719db6d4d8
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-10-24 10:37:50 +0200

    Show messages with the flow layout

--- Intercambio/Intercambio/ConversationViewController.swift
+++ Intercambio/Intercambio/ConversationViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 import Fountain
 
-class ConversationViewController: UIViewController, ConversationView {
+class ConversationViewController: UICollectionViewController, ConversationView {
 
     var eventHandler: ConversationViewEventHandler?
     var dataSource: FTDataSource? {
         didSet {
-            
+            collectionViewAdapter?.dataSource = dataSource
         }
     }
     
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 100, height: 100)
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var collectionViewAdapter: FTCollectionViewAdapter?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        collectionViewAdapter = FTCollectionViewAdapter(collectionView: collectionView)
+        
+        collectionView?.register(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: "undefined")
+        collectionViewAdapter?.forItemsMatching(nil, useCellWithReuseIdentifier: "undefined", prepare: { (view, item, indexPath, dataSource) in
+            if  let cell = view as? UICollectionViewCell {
+                cell.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+            }
+        })
+        
+        collectionViewAdapter?.delegate = self
+        collectionViewAdapter?.dataSource = dataSource
+    }
 }
