589bfb84340cb0b0a6c251090247d911284e6f97
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-10-25 15:59:19 +0200

    Implement layout of the group header with the timestamp

--- Intercambio/Intercambio/ConversationViewGroupHeaderView.swift
+++ Intercambio/Intercambio/ConversationViewGroupHeaderView.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ConversationViewGroupHeaderView: UICollectionReusableView {
+    
+    static var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        formatter.doesRelativeDateFormatting = true
+        return formatter
+    }()
+    
+    let label: UILabel
+    
     override init(frame: CGRect) {
+        label = UILabel()
         super.init(frame: frame)
-        backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        
+        label.frame = bounds
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        label.font = UIFont.preferredFont(forTextStyle: .caption2)
+        label.textAlignment = .center
+        label.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        
+        addSubview(label)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        if let attributes = layoutAttributes as? ConversationViewLayoutAttributes {
+            if let timestamp = attributes.timestamp {
+                let formatter = ConversationViewGroupHeaderView.dateFormatter
+                label.text = formatter.string(for: timestamp)
+            } else {
+                label.text = nil
+            }
+        }
+        super.apply(layoutAttributes)
     }
 }
