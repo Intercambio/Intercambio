27eeb99983762b70af23cbda97c42c449bdbcc35
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-02-01 10:29:35 +0100

    Add target for IntercambioCore

---
+++ Intercambio/IntercambioCore/IntercambioCore.h
@@ -0,0 +1,19 @@
+//
+//  IntercambioCore.h
+//  IntercambioCore
+//
+//  Created by Tobias Kraentzer on 01.02.17.
+//  Copyright © 2017 Tobias Kräntzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IntercambioCore.
+FOUNDATION_EXPORT double IntercambioCoreVersionNumber;
+
+//! Project version string for IntercambioCore.
+FOUNDATION_EXPORT const unsigned char IntercambioCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntercambioCore/PublicHeader.h>
+
+
